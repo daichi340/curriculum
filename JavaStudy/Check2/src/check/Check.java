b60f5cda283f94da1826fc9c790a11647e5c2732
daichi340 <konosuba3401@gmail.com>
2020-10-21 23:50:20 +0900

    課題提出
    check課題提出

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,21 @@
+package check;
+
+import constants.Constants;
+
+public class Check {
+ private static String firstName = "見塩";
+ private String lastName = "大地";
+
+ private void printName(String firstName,String lastName) {
+     System.out.println("printNameメソッド　→　" + Check.firstName + this.lastName);
+	}
+public static void main(String[] args) {
+	Check check = new Check();
+	Pet p = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+	RobotPet rp = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
+    check.printName(firstName, firstName);
+    p.introduce();
+    rp.introduce();
+
+}
+}
